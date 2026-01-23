cab4285fcc9d21da3c7facd503f56d25e810b91b
Shweta Gudase <shwetagudse@gmail.com>
2026-01-23 11:09:57 +0530

    Finish leftover logic

---
+++ JavaScript/ClassActivity/Lec 2/lec2Activity.js
@@ -0,0 +1,55 @@
+//Activity 1: conversion of all data types
+//convert number into string
+let num = 33;
+let string_num = String(num);
+console.log(string_num)
+console.log("The type of string_num : ",typeof(string_num))
+
+//convert string into number
+let str_num = "33";
+let number = Number(str_num);
+console.log(number)
+console.log("The type of number is : ",typeof(number))
+
+//convert boolean into number
+let bool_val = true;
+let bool_to_num = Number(bool_val);
+console.log(bool_to_num)
+console.log("The type of bool_to_num is : ",typeof(bool_to_num))
+
+//convert boolean into string
+let bool_to_str = String(bool_val);
+console.log(bool_to_str)
+console.log("The type of bool_to_str is : ",typeof(bool_to_str))
+
+//convert string into boolean
+let str_val = "Hello";
+let str_to_bool = Boolean(str_val);
+console.log(str_to_bool)
+console.log("The type of str_to_bool is : ",typeof(str_to_bool))
+
+//convert number into boolean
+let num_val = 45;
+let num_to_bool = Boolean(num_val);
+console.log(num_to_bool)
+console.log("The type of num_to_bool is : ",typeof(num_to_bool))
+
+//convert empty string into number
+let empty_str = "";
+let emptystr_to_num = Number(empty_str);
+console.log(emptystr_to_num)
+console.log("The type of emptystr_to_num is : ",typeof(emptystr_to_num))
+
+
+//Activity 2:
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+//detail study on this
+
+
+
+
+
+
+
