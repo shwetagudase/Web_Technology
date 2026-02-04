d5b589270fd1a4fd314b13d369197c990c646968
Shweta Gudase <shwetagudse@gmail.com>
2026-02-04 19:42:40 +0530

    Enhanced project features

--- JavaScript/ClassActivity/Lec 6/Project.js
+++ JavaScript/ClassActivity/Lec 6/Project.js
@@ -1,4 +1,11 @@
  function changeText() {
- let message =document.getElementById("msg").innerHTML="Welcome to JS DOM!";
- message.style.backgrounColor="red";
- }
+  let message = document.getElementById("msg");
+  message.innerHTML = "Welcome to JS DOM!";
+  message.style.color = "blue";
+}
+
+function changeColor() {
+  let message = document.getElementById("msg");
+  message.style.backgroundColor = "red";
+  message.style.padding="20px"
+}
